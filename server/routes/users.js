ff8e729c29fca26394907174510b1a4aa40087a2
Anushka <spacetasticnova@gmail.com>
2025-07-23 23:52:21 +0530

    Updated backend URL

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,14 @@ const User = require('../models/User');
 
 router.get('/', async (req, res) => {
   const users = await User.find();
+  console.log('Users fetched:', users);
   res.json(users);
 });
 
 router.post('/', async (req, res) => {
   const user = new User({ name: req.body.name });
   await user.save();
+  console.log('New user created:', user);
   res.json(user);
 });
 

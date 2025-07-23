ccbef98d4f41bac4265b91be5258358b78001435
Anushka <spacetasticnova@gmail.com>
2025-07-23 23:02:26 +0530

    Fixed CORS issue in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://leaderboardwebapplication.netlify.app/',
+  origin: 'https://leaderboardwebapplication.netlify.app',
   credentials: true
 }));
 
